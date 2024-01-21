eda51ca7190a42f67844ea07bf2868fb324728af
hansung1908 <hansung1908@naver.com>
2024-01-21 23:06:46 +0900

    item3 정리

---
+++ item3/finalfield/Elvis.java
@@ -0,0 +1,19 @@
+package item3.finalfield;
+
+// 코드 3-1 public static final 필드 방식의 싱글턴
+public class Elvis {
+    public static final Elvis INSTANCE = new Elvis();
+
+    // private 생성자는 public static final 필드인 Elvis.INSTANCE를 초기화할 때 딱 한 번만 호출된다.
+    private Elvis() {}
+
+    public void leaveTheBuilding() {
+        System.out.println("기다려 자기야, 지금 나갈께!");
+    }
+
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTANCE;
+        elvis.leaveTheBuilding();
+    }
+}
