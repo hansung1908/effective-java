ddf7e802aca89dd3bc261f2447a3a2f7bc75af0d
hansung1908 <hansung1908@naver.com>
2024-01-21 23:39:02 +0900

    item5 정리

---
+++ item5/singleton/SpellChecker.java
@@ -0,0 +1,21 @@
+package item5.singleton;
+
+import item5.Lexicon;
+import java.util.List;
+
+// 코드 5-2 싱글턴을 잘못 사용한 예 - 유연하지 않고 테스트하기 어렵다.
+public class SpellChecker {
+    private final Lexicon dictionary = new Lexicon();
+
+    private SpellChecker() {}
+    public static SpellChecker INSTANCE = new SpellChecker();
+
+    public static boolean isValid(String word) {
+        return true;
+    }
+
+    public static List<String> suggestions(String typo) {
+        List<String> result = List.of(typo);
+        return result;
+    }
+}
