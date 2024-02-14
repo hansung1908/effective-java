865d8f17d7f191826be765cbf8c7b26e8c249e5f
hansung1908 <hansung1908@naver.com>
2024-02-14 22:49:47 +0900

    item17 정리

---
+++ chapter4/item17/Complex.java
@@ -0,0 +1,63 @@
+package chapter4.item17;
+
+// 코드 17-1 불변 복소수 클래스 (106-107쪽)
+public final class Complex {
+    private final double re;
+    private final double im;
+
+    // 불변 클래스라면 한번 만든 인스턴스를 최대한 재활용하기를 권한다.
+    // 가장 쉬운 방법은 자주 쓰이는 값들을 상수(public static final)로 제공하는 것이다.
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE  = new Complex(1, 0);
+    public static final Complex I    = new Complex(0, 1);
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double realPart()      { return re; }
+    public double imaginaryPart() { return im; }
+
+    public Complex plus(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    // 코드 17-2 정적 팩터리(private 생성자와 함께 사용해야 한다.) (110-111쪽)
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public Complex minus(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex times(Complex c) {
+        return new Complex(re * c.re - im * c.im,
+                re * c.im + im * c.re);
+    }
+
+    public Complex dividedBy(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp,
+                (im * c.re - re * c.im) / tmp);
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Complex))
+            return false;
+        Complex c = (Complex) o;
+
+        return Double.compare(c.re, re) == 0
+                && Double.compare(c.im, im) == 0;
+    }
+    @Override public int hashCode() {
+        return 31 * Double.hashCode(re) + Double.hashCode(im);
+    }
+
+    @Override public String toString() {
+        return "(" + re + " + " + im + "i)";
+    }
+}
