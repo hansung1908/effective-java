6d71ec7236ddbd518250fde5f5c975ec764c96db
hansung1908 <hansung1908@naver.com>
2024-02-28 21:37:41 +0900

    item30 정리 + readme 수정

---
+++ chapter5/item30/RecursiveTypeBound.java
@@ -0,0 +1,25 @@
+package chapter5.item30;
+
+import java.util.*;
+
+// 재귀적 타입 한정을 이용해 상호 비교할 수 있음을 표현
+public class RecursiveTypeBound {
+    // 코드 30-7 컬렉션에서 최댓값을 반환한다. - 재귀적 타입 한정 사용
+    public static <E extends Comparable<E>> E max(Collection<E> c) {
+        if (c.isEmpty())
+            throw new IllegalArgumentException("컬렉션이 비어 있습니다.");
+
+        E result = null;
+        for (E e : c)
+            if (result == null || e.compareTo(result) > 0)
+                result = Objects.requireNonNull(e);
+
+        return result;
+    }
+
+    // 빈 컬렉션을 건네면 IllegalArgumentException을 던지니, Optional<E>를 반환하도록 고치는 편이 나을 것이다.
+    public static void main(String[] args) {
+        List<String> argList = Arrays.asList(args);
+        System.out.println(max(argList));
+    }
+}
